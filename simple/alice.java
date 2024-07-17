bd2fcf705c70879558b5402cbee8414965aff6ff
apartha2000 <arun.partha@truelark.com>
2024-07-17 10:11:59 -0700

    Create alice.java

---
+++ simple/alice.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+public static void main(String[] args) {
+    System.out.println("Hello, world!");
+    int a = 10;
+    int b= 20;
+    if (a<b) {
+    System.out.println(a + " is less than " + b);
+    } else {System.out.println(a + " is not less than " + b);
+    }
+}
