9d720540cb78ce5ae852259b68e1754336a53585
apartha2000 <arun.partha@truelark.com>
2024-07-23 10:20:56 -0700

    Update alice.java

--- simple/alice.java
+++ simple/alice.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 public static void main(String[] args) {
     System.out.println("Hello, world!");
     int a = 10;
-    int b= 20;    sd
+    int b= 20;    sgd
     if (a<b) {
     System.out.println(a + " is less than " + b);
     } else {System.out.println(a + " is not less than " + b);
